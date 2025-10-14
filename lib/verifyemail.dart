8bc150024d4fdc707ece532fbac7a0e7a216ae74
Racheet Bedi <k1328653@students.katyisd.org>
2025-10-14 14:35:11 +0000

    Add verifyemail class.

---
+++ lib/verifyemail.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_app/forgot.dart';
+import 'package:flutter_app/signup.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+
+class Verify extends StatefulWidget {
+  const Verify({super.key});
+
+  @override
+  State<Verify> createState() => _VerifyState();
+}
+
+class _VerifyState extends State<Verify> {
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  sendverifylink() async{
+    final user = FirebaseAuth.instance.currentUser;
+    await user.sendEmailVerification().then(value)=>{
+      Get.snackbar('Link sent', 'A link has been sent to your email.', margin: )
+    }
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
