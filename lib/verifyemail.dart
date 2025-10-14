632a17213b830e029f9cceb653a32c3fc6d9f3bb
Racheet Bedi <k1328653@students.katyisd.org>
2025-10-14 14:54:25 +0000

    Added verifyemail changes.

--- lib/verifyemail.dart
+++ lib/verifyemail.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_app/forgot.dart';
 import 'package:flutter_app/signup.dart';
+import 'package:flutter_app/wrapper.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_navigation/get_navigation.dart';
 
@@ -21,11 +22,16 @@ class _VerifyState extends State<Verify> {
 
   sendverifylink() async{
     final user = FirebaseAuth.instance.currentUser;
-    await user.sendEmailVerification().then(value)=>{
-      Get.snackbar('Link sent', 'A link has been sent to your email.', margin: )
-    }
+    await user?.sendEmailVerification().then((value)=>{
+      Get.snackbar('Link sent', 'A link has been sent to your email.', margin: EdgeInsets.all(30), snackPosition: SnackPosition.BOTTOM)
+    });
   }
 
+  reload() async{
+    await FirebaseAuth.instance.currentUser!.reload().then((value)=> {
+      Get.offAll(Wrapper()),
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
