e1df336703be26c19f710088c257b601001f7272
JeevanthJawaghar <K1827776@students.katyisd.org>
2025-10-14 14:51:39 +0000

    Added Text Field Decoration (Need to check if it works once Racheet fixes errors)

--- lib/theme.dart
+++ lib/theme.dart
@@ -4,8 +4,8 @@ final ThemeData appTheme = ThemeData(
     fontFamily: 'JosefinSlab-Bold',
     primaryColor: const Color(0xFFFFDD97),
     scaffoldBackgroundColor: const Color(0xFFFFDD97),
-    elevatedButtonTheme: ElevatedButtonThemeData(
-      
+
+    elevatedButtonTheme: ElevatedButtonThemeData(   
       style: ElevatedButton.styleFrom(
         backgroundColor: const Color.fromARGB(255, 0, 0, 0),
         foregroundColor: Colors.white,
@@ -17,6 +17,24 @@ final ThemeData appTheme = ThemeData(
           fontSize: 18,
           fontFamily: 'JosefinSlab-light',
         ),
+      ),
+    ),
+
+    inputDecorationTheme: InputDecorationTheme(
+      filled: true,
+      fillColor: Colors.white,
+      hintStyle: const TextStyle(
+        color: Color(0xFF828282),
+        fontFamily: 'JosefinSlab-light',
+      ),
+      contentPadding: const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(8),
+        borderSide: BorderSide.none,
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(8),
+        borderSide: const BorderSide(color: Colors.black, width: 2),
       ),
     ),
 );
