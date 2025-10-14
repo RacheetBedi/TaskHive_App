337a62a9bb0732b1e4bc21162ef5269ec5abd241
JeevanthJawaghar <K1827776@students.katyisd.org>
2025-10-14 14:39:18 +0000

    Elevated Button Font Update

--- lib/theme.dart
+++ lib/theme.dart
@@ -5,17 +5,17 @@ final ThemeData appTheme = ThemeData(
     primaryColor: const Color(0xFFFFDD97),
     scaffoldBackgroundColor: const Color(0xFFFFDD97),
     elevatedButtonTheme: ElevatedButtonThemeData(
+      
       style: ElevatedButton.styleFrom(
         backgroundColor: const Color.fromARGB(255, 0, 0, 0),
         foregroundColor: Colors.white,
-        fontFamily: 'JosefinSlab-Light',
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(30),
         ),
         padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 30),
         textStyle: const TextStyle(
           fontSize: 18,
-          fontWeight: FontWeight.bold,
+          fontFamily: 'JosefinSlab-light',
         ),
       ),
     ),
