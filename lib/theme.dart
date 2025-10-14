19f1ee574a71424cb8dbaf8d6e8e265cbd248005
JeevanthJawaghar <K1827776@students.katyisd.org>
2025-10-14 14:34:54 +0000

    Theme Data Update

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,5 +1,22 @@
 import 'package:flutter/material.dart';
 
 final ThemeData appTheme = ThemeData(
+    fontFamily: 'JosefinSlab-Bold',
     primaryColor: const Color(0xFFFFDD97),
+    scaffoldBackgroundColor: const Color(0xFFFFDD97),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+        foregroundColor: Colors.white,
+        fontFamily: 'JosefinSlab-Light',
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30),
+        ),
+        padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+        textStyle: const TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    ),
 );
