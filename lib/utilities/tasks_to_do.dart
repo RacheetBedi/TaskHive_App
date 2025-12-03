6cfd4f7e85e6929669cf6750d2db6d21d9d77f39
Racheet Bedi <k1328653@students.katyisd.org>
2025-12-03 15:10:04 +0000

    Created toDoList

---
+++ lib/utilities/tasks_to_do.dart
@@ -0,0 +1,25 @@
+
+//Racheet's Tasks
+//update appUser fields in both appUser and userRepository
+// migrate homepage to home
+// Fix login issues
+// Add signUp error handling
+// Add document creation for teachers (NEXT TO NEXT, NOT NEXT, RUBRIC)
+// Delete account code
+// Settings profile button
+// Edit tasks button
+// Fix signUpAsStudent back button functionality
+// Make the "I don't see my school" popup
+// Fix copy paste functionality (with a copied or pasted snackbar when the copy/paste is executed)
+// Change all hasCompletedSetup references to hasCreatedDocument
+// Make it so that signUp takes you to a separate page involving final account creation tasks such as verifying your username is allowed, etc...
+// Make the https call safe
+// Add Firebase and Firestore security rules
+// Add possessions for teachers
+// Create a possessions collection with the necessary documents for each student user.
+// Fix nameCase and make it standardized throughout the Firestore database
+// Add each username to a separate collection for easy searching
+// Encrypt the password
+// Tasks for Jeevanth:
+//    When you select a text field during log in or sign up, the text field outlines itself.
+//    Make the password rules on student and teacher sign-in look good.
